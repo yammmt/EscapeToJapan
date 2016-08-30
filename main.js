811c5e8aa9a5310b737ee7c702439a45973cb5dd
y_mac <cu.cs.y20@gmail.com>
2016-08-30 21:16:07 +0900

    add roulette (doesn't stop) and change some var name

--- main.js
+++ main.js
@@ -1,68 +1,102 @@
-// JP prefs list
-// TODO: more plain pref's format (using DB?)
-const prefs_hokkaido = ["北海道"];
-const prefs_tohoku = ["青森県", "岩手県", "秋田県", "宮城県", "山形県", "福島県"];
-const prefs_kanto = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"];
-const prefs_chubu = ["山梨県", "長野県", "新潟県", "富山県", "石川県", "福井県", "静岡県", "愛知県", "岐阜県"];
-const prefs_kinki = ["三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"];
-const prefs_chugoku = ["鳥取県", "島根県", "岡山県", "広島県", "山口県"];
-const prefs_shikoku = ["香川県", "愛媛県", "徳島県", "高知県"];
-const prefs_kyushu = ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"];
-var prefs_all = new Array();
+// JP pref list
+// TODO: more plain format (using DB?)
+const pref_hokkaido = ["北海道"];
+const pref_tohoku = ["青森県", "岩手県", "秋田県", "宮城県", "山形県", "福島県"];
+const pref_kanto = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"];
+const pref_chubu = ["山梨県", "長野県", "新潟県", "富山県", "石川県", "福井県", "静岡県", "愛知県", "岐阜県"];
+const pref_kinki = ["三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"];
+const pref_chugoku = ["鳥取県", "島根県", "岡山県", "広島県", "山口県"];
+const pref_shikoku = ["香川県", "愛媛県", "徳島県", "高知県"];
+const pref_kyushu = ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"];
+var pref_all = new Array();
 
-// Bootstrap className define
+// Bootstrap button className define
 // choices: default, primary, success, info, warning, danger, link
-const btn_classname_selected = 'btn btn-primary'
-const btn_classname_not_selected = 'btn btn-default';
+const btn_classname_can_be_selected = 'btn btn-primary'
+const btn_classname_can_not_be_selected = 'btn btn-default';
+
+// const parameters
+const roulette_speed = 250; // [ms]
+
+// var index array
+var pref_idx = new Array();
+
+// other global var
+var pref_elem; // used as "pref_elem[pref_idx[i]]""
+var now_selected = 0;
 
 function changeButtonStatus(e) {
-  //console.log(e.name);
-  if(e.className == btn_classname_selected) {
-    e.className = btn_classname_not_selected;
+  if(e.className == btn_classname_can_be_selected) {
+    e.className = btn_classname_can_not_be_selected;
   }
   else {
-    e.className = btn_classname_selected;
+    e.className = btn_classname_can_be_selected;
   }
+}
+
+// TODO: create this function
+function moveRoulette() {
+  var idx_old = now_selected-1;
+  if(idx_old < 0) {
+    idx_old = pref_idx.length-1;
+  }
+  // console.log("pref_idx.length:" + pref_idx.length);
+  // console.log("idx_old:" + idx_old);
+  // console.log("pref_idx[idx_old]:" + pref_idx[idx_old]);
+  pref_elem[pref_idx[idx_old]].className = btn_classname_can_be_selected;
+  pref_elem[pref_idx[now_selected]].className = "btn btn-warning";
+  now_selected = (now_selected+1)%pref_idx.length;
+
+  setTimeout(moveRoulette, roulette_speed);
 }
 
 function startRoulette(e) {
-  e.disabled = true;
-  var elem = document.getElementsByName("prefs");
-  for(var i=0; i<prefs_all.length; i++) {
-    if(elem[i].className == btn_classname_not_selected) {
+  //e.disabled = true;
+  var elem = document.getElementsByName("pref");
+  var length_org = pref_all.length;
+  for(var i=0; i<length_org; i++) {
+    if(elem[i].className == btn_classname_can_not_be_selected) {
       elem[i].disabled = true;
     }
+    else {
+      pref_idx.push(i);
+    }
   }
+  pref_elem = document.getElementsByName("pref");
+  moveRoulette();
 }
 
+// TODO: create this function
 function stopRoulette(e) {
+  //e.disabled = true;
+  window.alert("stop!");
 }
 
 function init() {
   // FIXME: this might be a very slow function
-  function mergePrefs() {
-    Array.prototype.push.apply(prefs_all, prefs_hokkaido);
-    Array.prototype.push.apply(prefs_all, prefs_tohoku);
-    Array.prototype.push.apply(prefs_all, prefs_kanto);
-    Array.prototype.push.apply(prefs_all, prefs_chubu);
-    Array.prototype.push.apply(prefs_all, prefs_kinki);
-    Array.prototype.push.apply(prefs_all, prefs_chugoku);
-    Array.prototype.push.apply(prefs_all, prefs_shikoku);
-    Array.prototype.push.apply(prefs_all, prefs_kyushu);
+  function mergepref() {
+    Array.prototype.push.apply(pref_all, pref_hokkaido);
+    Array.prototype.push.apply(pref_all, pref_tohoku);
+    Array.prototype.push.apply(pref_all, pref_kanto);
+    Array.prototype.push.apply(pref_all, pref_chubu);
+    Array.prototype.push.apply(pref_all, pref_kinki);
+    Array.prototype.push.apply(pref_all, pref_chugoku);
+    Array.prototype.push.apply(pref_all, pref_shikoku);
+    Array.prototype.push.apply(pref_all, pref_kyushu);
   }
 
   function createButtonHTML(btn_name) {
-    var html_created = "<button type='button' name='prefs' class='" + btn_classname_selected + "' onclick='changeButtonStatus(this)'>";
+    var html_created = "<button type='button' name='pref' class='" + btn_classname_can_be_selected + "' onclick='changeButtonStatus(this)'>";
     html_created += btn_name + "</button>";
     return html_created;
   }
 
-  mergePrefs();
+  mergepref();
 
   // TODO: more concrete and readable var name
-  prefs_btn_innerHTML = "";
-  for(var i=0; i<prefs_all.length; i++) {
-    prefs_btn_innerHTML += createButtonHTML(prefs_all[i]);
+  pref_btn_innerHTML = "";
+  for(var i=0; i<pref_all.length; i++) {
+    pref_btn_innerHTML += createButtonHTML(pref_all[i]);
   }
-  document.getElementById("prefs_btn").innerHTML = prefs_btn_innerHTML;
+  document.getElementById("pref_btn").innerHTML = pref_btn_innerHTML;
 }
